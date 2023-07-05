8e5e2007c398c27a7c389199f347d49b6b8de0ee
hoyyChoi <hoyeon8165@gmail.com>
2023-07-06 03:04:59 +0900

    feat : userLogin

---
+++ source/UserInfo.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import {Text, Box, useInput, Newline} from 'ink';
+import TextInput from 'ink-text-input';
+
+const UserInfo = ({setShow}) => {
+	const [nextStepInfo, setNextStepInfo] = useState(false);
+	const [Id, setId] = useState('');
+	const [Password, setPassword] = useState('');
+
+	useInput((input, key) => {
+		if (!key) return;
+
+		if (key.return) {
+			if (nextStepInfo) {
+				if (Id === 'hoyeon' && Password == 'hello') {
+					setShow(true);
+				} else {
+					process.exit(0); //아이디 비번이 틀릴경우 (임시)
+				}
+			}
+			setNextStepInfo(true);
+		} else if (input === 'c') {
+			process.exit(0);
+		}
+	});
+	//아이디 비번 받아서 서버랑 연결
+
+	return (
+		<Box marginY={1} flexDirection="column">
+			<Box>
+				<Text color="red">ID: </Text>
+				{nextStepInfo ? (
+					<Text>{Id}</Text>
+				) : (
+					<TextInput value={Id} onChange={setId} />
+				)}
+			</Box>
+			{nextStepInfo ? (
+				<Box>
+					<Text color="red">PASSWORD: </Text>
+					<TextInput value={Password} onChange={setPassword} />
+				</Box>
+			) : (
+				<Text>{Password}</Text>
+			)}
+		</Box>
+	);
+};
+
+export default UserInfo;
