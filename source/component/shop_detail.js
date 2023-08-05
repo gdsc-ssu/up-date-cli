4a054c41aa12ad97a5917b7d469d7de5ffc4b67c
Sanghyeon Sim <5338095@gmail.com>
2023-08-05 23:21:10 +0900

    refactor: apply prettier

--- source/component/shop_detail.js
+++ source/component/shop_detail.js
@@ -1,32 +1,66 @@
 import React from 'react';
-import { Text, Newline } from 'ink';
-import data from '../examples/shop.js';
+import {Text, Newline, Box} from 'ink';
 
-const ShopDetail = () => {
-	
-    return (
-		<Text>
-			<Text>• id : {data.id}<Newline /></Text>
-			<Text>• title : {data.title}<Newline /></Text>
-			<Text>• location : {data.location}<Newline /></Text>
-			<Text>• nearStation : {data.nearStation}<Newline /></Text>
-			<Text>• openTime : {data.openTime} ~ {data.closeTime}<Newline /></Text>
-			<Text>• menu : {"["}<Newline />
-				{data.menu.map(item => (
-					<Text key={item.name}>   {"{"} "{item.name}" : {item.price} {"},"}<Newline /></Text>
-				))}
-				{"]"}
-				<Newline />
-			</Text>
-			<Text>• starRate : {data.starRate}<Newline /></Text>
-			<Text>• reviews : {"["}<Newline/> 
-				{data.reviews.map(item=> (
-					<Text key={item.id}>   {"{"} "{item.writer}" : {item.content} ({item.starRate}) {"},"}<Newline /></Text>
-				))}
-				{"]"}
-				<Newline />
-			</Text>
-		</Text>
-    );
-}
+const ShopDetail = ({data}) => {
+	const starRateRounded = Math.round(data.starRate);
+
+	const starRateString = '⭐'.repeat(starRateRounded);
+
+	return (
+		<>
+			<Text>{'{'}</Text>
+			<Box marginLeft={2}>
+				<Text>
+					<Text>
+						id : {data.id},<Newline />
+					</Text>
+					<Text>
+						title : {data.title},<Newline />
+					</Text>
+					<Text marginLeft={2}>
+						location : {data.location},<Newline />
+					</Text>
+					<Text>
+						nearStation : {data.nearStation},<Newline />
+					</Text>
+					<Text>
+						openTime : {data.openTime} ~ {data.closeTime},<Newline />
+					</Text>
+					<Text>
+						menu : {'['}
+						<Newline />
+						{data.menu.map((item, index, array) => (
+							<Text key={item.name}>
+								{' '}
+								{'{'} "{item.name}" : {item.price} {'}'}
+								{index !== array.length - 1 ? ',' : ''}
+								<Newline />
+							</Text>
+						))}
+						{']'},
+						<Newline />
+					</Text>
+					<Text>
+						starRate : {starRateString}({data.starRate}),
+						<Newline />
+					</Text>
+					<Text>
+						reviews : {'['}
+						<Newline />
+						{data.reviews.map((item, index, array) => (
+							<Text key={item.id}>
+								{' '}
+								{'{'} "{item.writer}" : {item.content} ({item.starRate}) {'}'}
+								{index !== array.length - 1 ? ',' : ''}
+								<Newline />
+							</Text>
+						))}
+						{']'}
+					</Text>
+				</Text>
+			</Box>
+			<Text>{'}'}</Text>
+		</>
+	);
+};
 export default ShopDetail;
