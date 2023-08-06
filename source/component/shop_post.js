695f200bbe01bdf6d9743c3418813290181e1497
Sanghyeon Sim <5338095@gmail.com>
2023-08-06 16:02:05 +0900

    feat: shop post component

--- source/component/shop_post.js
+++ source/component/shop_post.js
@@ -29,10 +29,15 @@ const ShopPost = () => {
 
 	const [openDayList, setOpenDayList] = useState([]); // 추가: 오픈 요일
 	const [openDayIndex, setOpenDayIndex] = useState(0); // 추가: 오픈 요일 인덱스
-	const dayList = ['월', '화', '수', '목', '금', '토', '일', '완료']; // 요일 리스트
+	const dayList = ['월', '화', '수', '목', '금', '토', '일', '']; // 요일 리스트
 
-	const [openTime, setOpenTime] = useState(''); // 추가: 오픈 시간
-	const [closeTime, setCloseTime] = useState(''); // 추가: 마감 시간
+	//openTimeHour, openTimeMinute, closeTimeHour, closeTimeMinute
+	const [openTimeHour, setOpenTimeHour] = useState(0);
+	const [openTimeMinute, setOpenTimeMinute] = useState(0);
+	const [closeTimeHour, setCloseTimeHour] = useState(0);
+	const [closeTimeMinute, setCloseTimeMinute] = useState(0);
+
+	const [timeFocus, setTimeFocus] = useState(0); // 0 for hour, 1 for minute
 
 	useInput((input, key) => {
 		if (!key) return;
@@ -46,6 +51,7 @@ const ShopPost = () => {
 			}
 		}
 
+		// 운영 요일 추가
 		if (inputStep == 1) {
 			if (key.tab) {
 				if (openDayIndex == dayList.length - 1) {
@@ -55,14 +61,27 @@ const ShopPost = () => {
 				}
 			}
 
+			if (key.rightArrow) {
+				if (openDayIndex == dayList.length - 1) {
+					setOpenDayIndex(0);
+				} else {
+					setOpenDayIndex(openDayIndex => openDayIndex + 1);
+				}
+			}
+
+			if (key.leftArrow) {
+				if (openDayIndex == 0) {
+					setOpenDayIndex(dayList.length - 1);
+				} else {
+					setOpenDayIndex(openDayIndex => openDayIndex - 1);
+				}
+			}
+
 			if (key.return) {
 				// 완료 버튼 누르면 다음 단계로
 				if (openDayIndex == dayList.length - 1) {
-					// 마지막 텍스트는 "완료"임으로 삭제
-					setOpenDayList(
-						openDayList.filter(day => day != dayList[openDayIndex]),
-					);
 					setInputStep(prevInputStep => prevInputStep + 1);
+					return;
 				}
 
 				// 오픈 요일 추가, 이미 추가된 요일이면 삭제
@@ -78,6 +97,61 @@ const ShopPost = () => {
 			return;
 		}
 
+		// 시작 시간
+		if (inputStep == 2) {
+			if (key.tab || key.rightArrow) {
+				setTimeFocus((timeFocus + 1) % 2);
+			}
+			if (key.leftArrow) {
+				setTimeFocus((timeFocus - 1 + 2) % 2);
+			}
+			if (key.upArrow) {
+				if (timeFocus == 0) {
+					// hour
+					setOpenTimeHour((openTimeHour + 1) % 24);
+				} else {
+					// minute
+					setOpenTimeMinute((openTimeMinute + 15) % 60);
+				}
+			}
+			if (key.downArrow) {
+				if (timeFocus == 0) {
+					// hour
+					setOpenTimeHour((openTimeHour - 1 + 24) % 24);
+				} else {
+					// minute
+					setOpenTimeMinute((openTimeMinute - 15 + 60) % 60);
+				}
+			}
+		}
+		// 종료 시간
+		if (inputStep == 3) {
+			if (key.tab || key.rightArrow) {
+				setTimeFocus((timeFocus + 1) % 2);
+			}
+			if (key.leftArrow) {
+				setTimeFocus((timeFocus - 1 + 2) % 2);
+			}
+			if (key.upArrow) {
+				if (timeFocus == 0) {
+					// hour
+					setCloseTimeHour((closeTimeHour + 1) % 24);
+				} else {
+					// minute
+					setCloseTimeMinute((closeTimeMinute + 15) % 60);
+				}
+			}
+			if (key.downArrow) {
+				if (timeFocus == 0) {
+					// hour
+					setCloseTimeHour((closeTimeHour - 1 + 24) % 24);
+				} else {
+					// minute
+					setCloseTimeMinute((closeTimeMinute - 15 + 60) % 60);
+				}
+			}
+		}
+
 		// 카카오 가게 데이터가 받아오기 전에 엔터 누르면 오류 발생
 		try {
 			if (key.return) {
@@ -89,8 +163,9 @@ const ShopPost = () => {
 				}
 
 				setInputStep(prevInputStep => prevInputStep + 1);
-				if (inputStep == 2) {
-					process.exit(0);
+
+				if (inputStep == 4) {
+					console.log('complete');
 				}
 			}
 		} catch (error) {
@@ -141,19 +216,27 @@ const ShopPost = () => {
 							<Newline />
 						</Text>
 						<Box>
-							<Text>openTime : </Text>
+							<Text>openDay : </Text>
 							{inputStep == 1 ? (
 								<Box>
-									<Box>
-										{dayList.map((day, index) =>
-											// isFocused, isSelected, day
-											dayContainer(
-												index === openDayIndex,
-												openDayList.includes(index),
-												day,
-											),
-										)}
-									</Box>
+									{dayList.map((day, index) =>
+										// isFocused, isSelected, day
+										dayContainer(
+											index === openDayIndex,
+											openDayList.includes(dayList[index]),
+											day,
+										),
+									)}
+									<Text>
+										<Newline />
+										<Text
+											color={
+												openDayIndex === dayList.length - 1 ? 'yellow' : 'white'
+											}
+										>
+											{' 완료'}
+										</Text>
+									</Text>
 								</Box>
 							) : (
 								<Text>{openDayList.join(', ')}</Text>
@@ -161,16 +244,49 @@ const ShopPost = () => {
 						</Box>
 					</Box>
 				)}
+
+				{/* 오픈 시간 */}
 				{inputStep > 1 && (
 					<Box>
-						<Text>closeTime : </Text>
+						<Text>openTime : </Text>
 						{inputStep == 2 ? (
-							<TextInput
-								value={closeTime}
-								onChange={value => setCloseTime(value)}
-							/>
+							<Text>
+								<Text color={timeFocus == 0 ? 'yellow' : 'white'}>
+									{openTimeHour.toString().padStart(2, '0')}
+								</Text>
+								:
+								<Text color={timeFocus == 1 ? 'yellow' : 'white'}>
+									{openTimeMinute.toString().padStart(2, '0')}
+								</Text>
+							</Text>
 						) : (
-							<Text>{closeTime}</Text>
+							<Text>
+								{openTimeHour.toString().padStart(2, '0')}:
+								{openTimeMinute.toString().padStart(2, '0')}
+							</Text>
+						)}
+					</Box>
+				)}
+
+				{/* 마감 시간 */}
+				{inputStep > 2 && (
+					<Box>
+						<Text>closeTime : </Text>
+						{inputStep == 3 ? (
+							<Text>
+								<Text color={timeFocus == 0 ? 'yellow' : 'white'}>
+									{closeTimeHour.toString().padStart(2, '0')}
+								</Text>
+								:
+								<Text color={timeFocus == 1 ? 'yellow' : 'white'}>
+									{closeTimeMinute.toString().padStart(2, '0')}
+								</Text>
+							</Text>
+						) : (
+							<Text>
+								{closeTimeHour.toString().padStart(2, '0')}:
+								{closeTimeMinute.toString().padStart(2, '0')}
+							</Text>
 						)}
 					</Box>
 				)}
