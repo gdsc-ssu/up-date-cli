90c5e7bfd27776de2fbca6130fed9e97a7c166ae
hoyyChoi <hoyeon8165@gmail.com>
2023-08-19 20:20:13 +0900

    fix : Output -> seperate component

--- source/Output.js
+++ source/Output.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {Text, Box, useInput, Newline} from 'ink';
 import TextInput from 'ink-text-input';
 import theme from './Theme.js';
+import ListSubway from './component/ListSubway.js';
+import ListShop from './component/ListShop.js';
 //[[['신촌','강남','숭실대입구'],ls]]
 
 // [[[{},{}],'맛집']]
@@ -13,59 +15,8 @@ const Output = ({list}) => {
 					<Text color={theme.purple}>$ {item[1]} </Text>
 					<Newline />
 					{item[1] === 'ls'
-						? item[0].map((data, key) => (
-								<React.Fragment key={key}>
-									<Text bold color={theme.neonGreen}>
-										{item[1] === 'ls' ? data : data.original_title}
-										{/* {data || data.original_title} */}
-									</Text>
-									<Newline />
-								</React.Fragment>
-						  ))
-						: item[0].map((data, key) => (
-								<React.Fragment key={key}>
-									<Text>{'{'}</Text>
-									<Box marginLeft={2}>
-										<Text>
-											<Text>
-												"id" : "{data.id}",
-												<Newline />
-											</Text>
-											<Text>
-												"title" : "{data.title}",
-												<Newline />
-											</Text>
-											<Text marginLeft={2}>
-												"location" : "{data.location}",
-												<Newline />
-											</Text>
-											<Text>
-												"nearStation" : "{data.nearStation}",
-												<Newline />
-											</Text>
-											<Text>
-												"menu" : {'['}
-												<Newline />
-												{data.menu.map((item, index, array) => (
-													<Text key={item.name}>
-														{' '}
-														{'{'} "{item.name}" : {item.price} {'}'}
-														{index !== array.length - 1 ? ',' : ''}
-														<Newline />
-													</Text>
-												))}
-												{']'},
-												<Newline />
-											</Text>
-											<Text>
-												"starRate" : "{data.starRate}",
-												<Newline />
-											</Text>
-										</Text>
-									</Box>
-									<Text>{'}'}</Text>
-								</React.Fragment>
-						  ))}
+						? item[0].map((data, key) => <ListSubway data={data} key={key} />)
+						: item[0].map((data, key) => <ListShop data={data} key={key} />)}
 				</>
 			))}
 		</Box>
