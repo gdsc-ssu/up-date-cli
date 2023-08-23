4bd73214e1ea21ca1938aff5facc3c41abc3163c
Sanghyeon Sim <5338095@gmail.com>
2023-08-23 21:42:44 +0900

    feat: theme color add

--- source/Theme.js
+++ source/Theme.js
@@ -3,6 +3,16 @@ const theme = {
 	white: '#ffffff',
 	red: '#FF0000',
 	purple: '#AD00FF',
+	// 커맨드 여러개 선택 가능 할 때
+	commandFirst: '#FFD7400',
+	commandSecond: '#50bcdf',
+	commandThird: '#9E9EE8',
+	commandFourth: '#5CFFD1',
+	commandFifth: '#FF0000',
+	// 포커스 되었을 때
+	focused: '#FFF700',
+	selected: '#008000',
+	unfocused: '#ffffff',
 };
 
 export default theme;
