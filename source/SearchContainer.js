26f019621291a08d80e14c164dc7c5c46cfebb98
hoyyChoi <hoyeon8165@gmail.com>
2023-08-23 22:06:31 +0900

    feat : ListShop component

--- source/SearchContainer.js
+++ source/SearchContainer.js
@@ -5,16 +5,19 @@ import Output from './Output.js';
 import StationDetailType from './StationDetailType.js';
 import ShopDetail from './component/ShopDetail.js';
 import ShopPost from './component/ShopPost.js';
+import ListShop from './component/ListShop.js';
 
 const SearchContainer = () => {
 	const [list, setlist] = useState([]);
 	const [station, setStation] = useState(''); // 타이핑된 역의 이름
 	const [Id, setId] = useState('');
 	const [storeName, setStoreName] = useState('');
+	const [type, setType] = useState('');
+
 	return (
 		<Box marginY={1} flexDirection="column">
 			{
-				Id || storeName ? (
+				Id || storeName || type ? (
 					''
 				) : (
 					<Output list={list} />
@@ -23,10 +26,13 @@ const SearchContainer = () => {
 			{/* {aaa ? <ShopDetail /> : ''} */}
 			{station ? ( // 타이핑된 역의 이름이 없을 경우, 입력 창 유지, 입력했을시, 맛집, 액티비티, 선택창 나옴.
 				<StationDetailType
+					setType={setType}
 					station={station}
 					setlist={setlist}
 					setStation={setStation}
 				/>
+			) : type ? (
+				<ListShop setType={setType} />
 			) : Id ? (
 				<ShopDetail Id={Id} setId={setId} /> // 여기로 ID(나중엔 상세정보) 보내고, vi 탈출할때, setId값 초기화하면, 다시 커맨드 입력 창 나옴.
 			) : storeName ? (
