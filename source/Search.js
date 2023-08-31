0282b6cd9d65725b37e8e3d497fe4b0335625674
hoyyChoi <hoyeon8165@gmail.com>
2023-08-31 16:12:11 +0900

    remove : annotation

--- source/Search.js
+++ source/Search.js
@@ -74,7 +74,6 @@ const Search = ({setlist, setStation, setId, setStoreName, setSingleShop}) => {
 			if (search.startsWith('vi ')) {
 				let storeId = search.slice(3, search.length);
 				setlist(list => [...list, [[], search]]);
-				// 지금은 임시로 Id값을 저장하지만, 나중에 서버 구축이 되면, 여기서 api call 해서 가게 상세정보를 state값에 저장한다.
 				getSinglePlaceCheck(storeId).then(res => {
 					setSingleShop(res.data.body);
 					getReviewCheck(storeId).then(res => {
