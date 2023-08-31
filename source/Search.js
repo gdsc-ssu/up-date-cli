851a38fd39486980ef67f7084ea35f94bbbe52b3
hoyyChoi <hoyeon8165@gmail.com>
2023-08-31 15:44:11 +0900

    feat : change point of call

--- source/Search.js
+++ source/Search.js
@@ -74,9 +74,10 @@ const Search = ({setlist, setStation, setId, setStoreName, setSingleShop}) => {
 			if (search.startsWith('vi ')) {
 				let storeId = search.slice(3, search.length);
 				setlist(list => [...list, [[], search]]);
-				setId(storeId); // 지금은 임시로 Id값을 저장하지만, 나중에 서버 구축이 되면, 여기서 api call 해서 가게 상세정보를 state값에 저장한다.
+				// 지금은 임시로 Id값을 저장하지만, 나중에 서버 구축이 되면, 여기서 api call 해서 가게 상세정보를 state값에 저장한다.
 				getSinglePlaceCheck(storeId).then(res => {
 					setSingleShop(res.data.body);
+					setId(storeId);
 				});
 				setSearch('');
 				return;
